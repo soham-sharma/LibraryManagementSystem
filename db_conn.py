11ffe11ce3a68df7bfb359ee73d414301f912cf2
soham-sharma <sharm710@purdue.edu>
2024-04-26 02:44:37 -0400

    added cloud db support

--- db_conn.py
+++ db_conn.py
@@ -1,11 +1,20 @@
 import mysql.connector
-
+import streamlit as st
 
 def create_conn():
     conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="1234",
-        database="library",
+        host=st.secrets["database"]["host"],
+        user=st.secrets["database"]["user"],
+        password=st.secrets["database"]["password"],
+        database=st.secrets["database"]["database"],
     )
     return conn
+
+# main for testing
+if __name__ == "__main__":
+    conn = create_conn()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Books")
+    books = cursor.fetchall()
+    print(books)
+    conn.close()
