a0b4445cd2d1f325c10727787165ded4f797ae0d
soham-sharma <sharm710@purdue.edu>
2024-04-26 03:01:12 -0400

    updated db connection code

--- db_conn.py
+++ db_conn.py
@@ -1,11 +1,16 @@
 import mysql.connector
 import streamlit as st
 
+# Create a global connection object
+conn = None
+
 def create_conn():
-    conn = mysql.connector.connect(
-        host=st.secrets["database"]["host"],
-        user=st.secrets["database"]["user"],
-        password=st.secrets["database"]["password"],
-        database=st.secrets["database"]["database"],
-    )
+    global conn
+    if conn is None:
+        conn = mysql.connector.connect(
+            host=st.secrets["database"]["host"],
+            user=st.secrets["database"]["user"],
+            password=st.secrets["database"]["password"],
+            database=st.secrets["database"]["database"],
+        )
     return conn
